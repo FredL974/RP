abd8be6473cd3b842079dc84bfe5f5535d2378d9
tomtom06 <thomas.perles@gmail.com>
2017-01-11 18:52:03 +0100

    connect et deconnect

--- fr.ensisa.hassenforder.chatrooms.server.empty/src/fr/ensisa/hassenforder/chatrooms/server/CommandReader.java
+++ fr.ensisa.hassenforder.chatrooms.server.empty/src/fr/ensisa/hassenforder/chatrooms/server/CommandReader.java
@@ -1,7 +1,9 @@
 package fr.ensisa.hassenforder.chatrooms.server;
 
 import java.io.InputStream;
+import java.util.List;
 
+import fr.ensisa.hassenforder.chatrooms.server.model.Channel;
 import fr.ensisa.hassenforder.chatrooms.server.model.ChannelType;
 import fr.ensisa.hassenforder.network.BasicAbstractReader;
 import fr.ensisa.hassenforder.network.Protocol;
@@ -16,6 +18,7 @@ public class CommandReader extends BasicAbstractReader {
 	private CommandWriter w;
 	private int mode, length, valid, messageId, channelTypeNumber;
 	private ChannelType channelType;
+	private boolean subscription, approved;
 
 	public CommandReader(InputStream inputStream) {
 		super(inputStream);
@@ -26,13 +29,9 @@ public class CommandReader extends BasicAbstractReader {
 		switch (type) {
 		case Protocol.RQ_CONNECT:
 			this.userName = readString();
-			this.operationStatus = this.application.connectCommandUser(this.userName, this.commandSession);
-			this.w.sendOperationStatus(this.operationStatus);
 			break;
 		case Protocol.RQ_DISCONNECT:
 			this.userName = readString();
-			this.operationStatus = this.application.disconnectUser(this.userName);
-			this.w.sendOperationStatus(this.operationStatus);
 			break;
 		case Protocol.RQ_CREATEROOM:
 			this.userName = readString();
@@ -52,25 +51,30 @@ public class CommandReader extends BasicAbstractReader {
 			this.operationStatus = this.application.createChannel(this.userName, this.roomName, this.channelType);
 			this.w.sendOperationStatus(this.operationStatus);
 			break;
-		/*
-		 * 
-		 * case Protocol.RQ_LOADROOMS : this.userName = readString();
-		 * List<Channel> channels =
-		 * this.application.loadChannels(this.userName);
-		 * this.w.loadrooms(this.user, channels); break; case
-		 * Protocol.RQ_CHANNELSUBSCRIPTIONCHANGE : this.userName = readString();
-		 * this.roomName = readString(); this.subscription = readBoolean();
-		 * this.operationStatus =
-		 * this.application.ChangeChannelSubscription(this.userName,
-		 * this.roomName, this.subscription);
-		 * this.w.sendOperationStatus(this.operationStatus); break; case
-		 * Protocol.RQ_SETAPPROBATION : this.userName = readString();
-		 * this.messageId = readInt(); this.approved = readBoolean();
-		 * this.operationStatus = this.application.SetApprobation(this.userName,
-		 * this.messageId, this.approved);
-		 * this.w.sendOperationStatus(this.operationStatus); break;
-		 */
-
+		case Protocol.RQ_LOADROOMS :
+			this.userName = readString();
+			List<Channel> channels = this.application.loadChannels(this.userName);
+			this.w.loadChannel(this.userName, channels);
+			break;
+		case Protocol.RQ_CHANNELSUBSCRIPTIONCHANGE :
+			this.userName = readString();
+			this.roomName = readString();
+			this.subscription = readBoolean();
+			this.operationStatus = this.application.ChangeChannelSubscription(this.userName, this.roomName, this.subscription);
+			this.w.sendOperationStatus(this.operationStatus);
+			break;
+		case Protocol.RQ_SETAPPROBATION :
+			this.userName = readString();
+			this.messageId = readInt();
+			this.approved = readBoolean();
+			this.operationStatus = this.application.SetApprobation(this.userName, this.messageId, this.approved);
+			this.w.sendOperationStatus(this.operationStatus);
+			break;
 		}
+	}
+	
+	public String getName()
+	{
+		return userName;
 	}
 }
--- fr.ensisa.hassenforder.chatrooms.server.empty/src/fr/ensisa/hassenforder/chatrooms/server/CommandWriter.java
+++ fr.ensisa.hassenforder.chatrooms.server.empty/src/fr/ensisa/hassenforder/chatrooms/server/CommandWriter.java
@@ -1,18 +1,82 @@
 package fr.ensisa.hassenforder.chatrooms.server;
 
 import java.io.OutputStream;
+import java.util.List;
 
+import fr.ensisa.hassenforder.chatrooms.server.model.Channel;
 import fr.ensisa.hassenforder.network.BasicAbstractWriter;
+import fr.ensisa.hassenforder.network.Protocol;
 
 public class CommandWriter extends BasicAbstractWriter {
-
+	
+	private OperationStatus operationStatus;
+	
 	public CommandWriter(OutputStream outputStream) {
-		super (outputStream);
+		super(outputStream);
 	}
 
 	public void sendOperationStatus(OperationStatus operationStatus) {
+		// TODO
+		switch (operationStatus) {
+		case ALLREADY_CONNECTED:
+			System.out.println("Serveur CommandWriter sendOperationStatus");
+			
+			// TODO : voir BasicAbstractWriter
+			writeString("test"); 
+			
+			break;
+		case APPROBATION_CHANGED:
+			System.out.println("Serveur CommandWriter sendOperationStatus");
+			break;
+		case CHANNEL_CREATED:
+			System.out.println("Serveur CommandWriter sendOperationStatus");
+			break;
+		case CHANNEL_CREATION_FAILED:
+			System.out.println("Serveur CommandWriter sendOperationStatus");
+			break;
+		case CHANNEL_EXISTS:
+			System.out.println("Serveur CommandWriter sendOperationStatus");
+			break;
+		case MESSAGE_SENT:
+			System.out.println("Serveur CommandWriter sendOperationStatus");
+			break;
+		case NOT_CONNECTED:
+			System.out.println("Serveur CommandWriter sendOperationStatus");
+			break;
+		case NOW_CONNECTED:
+			System.out.println("Serveur CommandWriter sendOperationStatus");
+			break;
+		case NOW_DISCONNECTED:
+			System.out.println("Serveur CommandWriter sendOperationStatus");
+			break;
+		case SUBSCRIPTION_CHANGED:
+			System.out.println("Serveur CommandWriter sendOperationStatus");
+			break;
+		case UNKNOWN_CHANNEL:
+			System.out.println("Serveur CommandWriter sendOperationStatus");
+			break;
+		case UNKNOWN_MESSAGE:
+			System.out.println("Serveur CommandWriter sendOperationStatus");
+			break;
+		case UNKNOWN_USER:
+			System.out.println("Serveur CommandWriter sendOperationStatus");
+			break;
+		default:
+			System.out.println("Serveur CommandWriter sendOperationStatus");
+			break;
+		}
+	}
+
+	public void loadChannel(String userName, List<Channel> channels) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	public void writeOK() {
+		writeInt(Protocol.OK);
+	}
+
+	public void writeKO() {
+		writeInt(Protocol.KO);
+	}
 }
