fc534a233124b4fc5f94af1ff4065096dad63436
FredL974 <frederic.lusinier@gmail.com>
2017-01-11 08:22:47 +0100

    synch eclipse

---
+++ fr.ensisa.hassenforder.chatrooms.client.empty/src/fr/ensisa/hassenforder/chatrooms/client/CommandWriter.java
@@ -0,0 +1,19 @@
+package fr.ensisa.hassenforder.chatrooms.client;
+
+import java.io.OutputStream;
+
+import fr.ensisa.hassenforder.network.BasicAbstractWriter;
+import fr.ensisa.hassenforder.network.Protocol;
+
+public class CommandWriter extends BasicAbstractWriter {
+
+	public CommandWriter(OutputStream outputStream) {
+		super (outputStream);
+	}
+
+	public void createConnect(String name) {
+		writeInt(Protocol.RQ_CONNECT);
+		writeString(name);
+	}
+
+}
---
+++ fr.ensisa.hassenforder.chatrooms.server.empty/src/fr/ensisa/hassenforder/chatrooms/server/CommandReader.java
@@ -0,0 +1,24 @@
+package fr.ensisa.hassenforder.chatrooms.server;
+
+
+import java.io.InputStream;
+
+import fr.ensisa.hassenforder.network.BasicAbstractReader;
+import fr.ensisa.hassenforder.network.Protocol;
+
+public class CommandReader extends BasicAbstractReader {
+	private String name;
+	public CommandReader(InputStream inputStream) {
+		super (inputStream);
+	}
+
+	public void receive() {
+		type = readInt ();
+		switch (type) {
+		case Protocol.RQ_CONNECT:
+			name=readString();break;
+		case 0 : break;
+		}
+	}
+
+}
