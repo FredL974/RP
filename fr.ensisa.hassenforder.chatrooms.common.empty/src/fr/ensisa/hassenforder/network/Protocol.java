2058efcd165bac9f5256185737cfba72c16dd716
tomtom06 <thomas.perles@gmail.com>
2017-01-11 09:51:42 +0100

    client et protocol

--- fr.ensisa.hassenforder.chatrooms.common.empty/src/fr/ensisa/hassenforder/network/Protocol.java
+++ fr.ensisa.hassenforder.chatrooms.common.empty/src/fr/ensisa/hassenforder/network/Protocol.java
@@ -8,14 +8,20 @@ public interface Protocol {
 	public static final int OK = 254;	//commande demandée a réussi
 	public static final int KO = 255;	//commande demandée a échoué
 			//TODO
-	public static final int RQ_CONNECT = 1;					//(String user, boolean connected)
-	public static final int RQ_CREATEROOM = 2;				//(String roomName, short int mode, String moderatorName)
-	public static final int RQ_LOADROOMS = 3;				//()
+	public static final int RQ_CONNECT = 1;					//(String userName)
+	public static final int RQ_CREATEROOM = 2;				//(String moderatorName, string Channel,Channeltype type )
+	public static final int RQ_LOADROOMS = 3;				//(String name)
+	public static final int RQ_SETAPPROBATION = 4;			//(String name, int messageId, boolean approved)
+	/*
 	public static final int RQ_VALIDMODERATOR = 4;			//(String roomName, String userName, String message, short int valid)
 	public static final int RQ_INVALIDMODERATOR = 5;		//(String roomName, String userName, String message, short int valid)
-	public static final int RQ_POSTMESSAGE = 6;				//(String roomName, String userName, String message, short int valid)
+	*/
+	public static final int RQ_POSTMESSAGE = 6;				//(String userName ,String roomName , String message)
+	public static final int RQ_DISCONNECT = 7;				//(String userName)
+	public static final int RQ_CHANNELSUBSCRIPTIONCHANGE=8; //(String name, String roomName, boolean selected
+	public static final int RQ_MODERATIONSTATE=9;			//String name, message message,boolean approved
 	public static final int RP_CREATEROOMOK = 102;			//()
-	public static final int RP_LOADROOMSOK = 103;			//(short int length, struct [] room {String roomName, short int mode} )
+	public static final int RP_LOADROOMSOK = 103;			//(short int length, Room [] rooms // de la forme {String roomName; short int mode;} )
 	public static final int RQ_VALIDATIONMODERATOR = 104;	//(String roomName, String userName, String message, short int valid) avec valid = 1
 	public static final int RP_VALIDMODERATOROK = 104;		//(String roomName, String userName, String message, short int valid)
 	public static final int RP_INVALIDMODERATOROK = 105;	//(String roomName, String userName, String message, short int valid)
@@ -24,5 +30,5 @@ public interface Protocol {
 	public static final int RP_ERROR_STRINGFORBIDDEN = 201;
 	public static final int RP_ERROR_MEMORYSPACE = 202;
 	public static final int RP_ERROR_CONNECTION = 203;
-			//TODO
+			
 }
